4cc6df04ed4a4518543129df7f0791e35e6a0649
Kyn9x <h.kyn9x@gmail.com>
2016-04-02 05:13:54 +0700

    Update Veiga E

--- EvadePlus/SkillshotDatabase.cs
+++ EvadePlus/SkillshotDatabase.cs
@@ -2620,6 +2620,7 @@ namespace EvadePlus
                         DangerValue = 3,
                         IsDangerous = true,
                         MissileSpellName = "VeigarEventHorizon"
+                        ToggleParticleName = "Veiga_.+_E_tar_aoe_"
                     }
                 },
                 new CircularMissileSkillshot
